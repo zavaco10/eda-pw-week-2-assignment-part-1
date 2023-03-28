a2ab2c442eea95020a96d7d2dee53e9e6514407c
Cody Zavadil <zavaco10@gmail.com>
2023-03-28 12:42:54 -0500

    end of regular questions

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -18,28 +18,44 @@ console.log( 'My name is', fullName, 'and I think', luckyNumber, 'is a winner!' 
 // 7 - Create a variable named `adventurous` and set it to a boolean value (true or false)
 let adventurous = true
 // 8 - Create a variable named `food`, and set its value to a string of your favorite food
-
+let food = 'sausage';
 // 9 - Create a variable called `pets` and set it to the value of the number of pets you have
-
+let pets = 5
 // 10 - Create a variable called `friendsPets` and assign it the value of the number of pets your friend has
-
+let friendsPets = 2
 // 11 - Add two pets to your `pets` variable
-
+friendsPets += 2 
 // 12 - Create a constant variable called `allowedPets` and set it to a number value of your choice
-
+const allowedPets = 4
 // 13 - Create a conditional: if adventurous is true, console log "Adventures are great!", 
 // if it's not true, console log "How about we stay home?"
-
+if (adventurous == true) {
+    console.log ( 'Adventures are great!' );
+}
+else {
+    console.log( 'How about we stay home' );
+}
 
 // 14 - Create a compound conditional: if luckyNumber is 2 and adventurous is true,
 // console log "Roll the dice!"
+if (luckyNumber ==2 && adventurous == true){
+    console.log("Roll the dice!");
+}
 
 // 15 - Write a conditional that console logs "I can have more pets!" 
 //if the value of `pets` is less than the value of `allowedPets`,
 // console logs "I have enough pets" if the value of `pets` is equal to the value of `allowedPets`,
 // and console logs "Oh no, I have too many pets!"
 // if the value of `pets` is greater than the value of `allowedPets`.
-
+if (pets < allowedPets){
+console.log("I can have more pets!");
+}
+else if (pets == allowedPets){
+    console.log("I have enough pets");
+}
+else {
+console.log("Oh no, I have too many pets!");
+}
 // STRETCH GOALS:
 
 // 16 - Make a variable called `mostPets` and a conditional that
