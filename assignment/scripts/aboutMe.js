10fb06833f67f5eb640e84c651419775308b657b
Cody Zavadil <zavaco10@gmail.com>
2023-03-27 21:17:08 -0500

    after sentence

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -1,21 +1,22 @@
 // REQUIRED FEATURES:
 // 1 - Create a variable called `firstName` and assign it the value of your first name
-
+let firstName = 'Cody';
 // 2 - Create a second variable called `lastName` and assign it the value of your last name
-
+let lastName = "Zavadil"; 
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 // (remember, you already have variables for this, can you use those?)
-
+let fullName =  'Cody ' + lastName; 
 // 4 - Console log the value of `fullName`
-
+console.log(fullName); 
 // 5 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
-
+let luckyNumber = 2
 // 6 - Console log this sentence, adding in the variables you created above: 
 // 'My name is (full name), and I think (lucky number) is a winner!'.
 // Refer back to the videos if you need help with this one.
+console.log( 'My name is', fullName, 'and I think', luckyNumber, 'is a winner!' );
 
 // 7 - Create a variable named `adventurous` and set it to a boolean value (true or false)
-
+let adventurous = true
 // 8 - Create a variable named `food`, and set its value to a string of your favorite food
 
 // 9 - Create a variable called `pets` and set it to the value of the number of pets you have
